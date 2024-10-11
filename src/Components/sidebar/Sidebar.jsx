c9aab64fd6c805037619acd386c092f869c2299b
Naveed330 <na2885060@gmail.com>
2024-10-11 18:28:55 +0400

    11-10-2024
    11-10-2024

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../navbar/Navbar.css'
+import { Link } from 'react-router-dom'
 const Sidebar = () => {
     return (
         <div className='sidebar_main_container'>
-            <h5>sidebar Data</h5>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', justifyContent: 'center', alignItems: 'center' }} >
+                <Link to={'/leads'} style={{ textDecoration: 'none', color: 'black' }} className='mt-4' >Leads</Link>
+                <Link to={'/contract'} style={{ textDecoration: 'none', color: 'black' }} >Contract</Link>
+                <Link to={'/ceophonebook'} style={{ textDecoration: 'none', color: 'black' }} >CEO PhoneBook</Link>
+                <Link to={'/rejectedlead'} style={{ textDecoration: 'none', color: 'black' }} >Rejected Leads</Link>
+            </div>
         </div>
     )
 }
